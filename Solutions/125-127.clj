76174b5b1091da4500b8eae44e0541d1617a0f57
MoyTW <moytravis@gmail.com>
2014-02-10 18:32:21 -0800

    Solutions to 125-127.
    Really it's mostly 127...

---
+++ Solutions/125-127.clj
@@ -0,0 +1,46 @@
+;;;; -----=====***** 125 (1 line, 1 total) *****=====-----
+(fn [] (let [x ["(fn [] (let [x " "] (str (first x) x (last x))))"]] (str (first x) x (last x))))
+
+;;;; -----=====***** 126 (1 line, 2 total) *****=====-----
+java.lang.Class
+
+;;;; -----=====***** 127 (39 lines, 41 total) *****=====-----
+(fn jesus-wept [bitmap]
+  (letfn [(int-to-array [i c]
+            (->> (Integer/parseInt i)
+                 (format (str \% \0 c \d))
+                 (map #(= \1 %))
+                 (into [])))
+          (ints-to-array [bitmap]
+            (let [strs (map #(Integer/toBinaryString %) bitmap)
+                  pad-to (apply max (map count strs))]
+              (into [] (map #(int-to-array % pad-to) strs))))
+          (next-coords [coords]
+            (let [c (ffirst coords)
+                  r-min (last (first coords))
+                  r-max (last (last coords))]
+              (for [r (range (dec r-min) (+ 2 r-max))]
+                [(dec c) r])))
+          (largest-vertical [coords mine n]
+            (if (every? true? (map #(get-in mine %) coords))
+                (recur (next-coords coords) mine (+ n (count coords)))
+                n))
+          (next-ne [coords]
+            (into #{} (mapcat (fn [[r c]] [[(dec r) c] [r (inc c)]]) coords)))
+          (largest-diagonal [coords mine n]
+            (if (every? true? (map #(get-in mine %) coords))
+                (recur (next-ne coords) mine (+ n (count coords)))
+                n))
+          (gen-mine-permutations [mine]
+            [mine
+             (into [] (map (comp vec reverse) (apply map vector mine)))
+             (into [] (reverse (map (comp vec reverse) mine)))
+             (into [] (reverse (apply map vector mine)))])
+          (find-max [coord mine]
+            (max (largest-diagonal [coord] mine 0)
+                 (largest-vertical [coord] mine 0)))]
+    (let [mine (ints-to-array bitmap)
+          all-coords (for [r (range (count mine)) c (range (count (first mine)))] [r c])
+          all-mines (gen-mine-permutations mine)
+          area (apply max (for [c all-coords m all-mines] (find-max c m)))]
+      (if (> area 2) area))))
