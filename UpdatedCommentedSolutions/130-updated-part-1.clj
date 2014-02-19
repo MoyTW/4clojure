542e8e29e66058095096b7a5d87f357722b84055
MoyTW <moytravis@gmail.com>
2014-02-18 20:55:31 -0800

    Whoops. Forgot to break'm.
    I'll put it right up.

--- UpdatedCommentedSolutions/130-updated-part-1.clj
+++ UpdatedCommentedSolutions/130-updated-part-1.clj
@@ -1,5 +1,5 @@
 ;;;; 130 - Tree reparenting
-;;; Scratch: https://github.com/MoyTW/4clojure/blob/master/ScratchPaper/130-wip.clj
+ ;;; Scratch:;;   https://github.com/MoyTW/4clojure/blob/master/ScratchPaper/130-wip.clj
 ;; Original:
 (fn bah [target tree]
   (letfn [(to-map-step [parent [key & children] m]
@@ -46,7 +46,9 @@
          (pull-down)
          (to-list))))
 ;; This one was a real doozy.
-;; The way in which I chose to solve it was...inelegant. It works with a different internal representation than the given representation - instead of a nested list of lists, it uses a map of nodes, like so:
+;;   The way in which I chose to solve it was...inelegant. It works with a
+;; different internal representation than the given representation - instead of
+;; a nested list of lists, it uses a map of nodes, like so:
 ;;   {a {:parents [], :children [b f]},
 ;;    b {:children (c e), :parents [a d]},
 ;;    c {:parents [b], :children []},
@@ -55,11 +57,24 @@
 ;;    f {:children [g h], :parents [a]},
 ;;    g {:parents [f], :children []},
 ;;    h {:parents [f], :children []}}
-;; When it processes the list, what it does is finds the node that it wants to pull up, clears out the :parents element of that node, and then adds to its :children node the former parent. The former parent on the other hand has the target node added to its :parents element.
-;; Then, in the second pass, the multi-parented node 'pushes' its new parent downwards, to become a child of the node, again by screwing around with the :parents and :children elements of the two.
+;;   When it processes the list, what it does is finds the node that it wants to
+;; pull up, clears out the :parents element of that node, and then adds to its
+;; :children node the former parent. The former parent on the other hand has the
+;; target node added to its :parents element.
+;;   Then, in the second pass, the multi-parented node 'pushes' its new parent
+;; downwards, to become a child of the node, again by screwing around with the
+;; :parents and :children elements of the two.
 ;;   Finally, it converts the nodes back to a list representation.
-;;   Wow, that's convoluted. Okay, well, let's ignore the code to convert to and from the tree and concentrate on the actual manipulations. Specifically, let's look at swap-positions and push-node.
-;; One thing to note is that we can actually merge these two functions. The "Find the thing with multiple parents" code will always turn up the node which was formerly the parent of the node we want to pull up to the top, so we can actually do away with that and directly resolve all three modifications in one step. Well, sort of - it's complicated by the fact that sometimes pulling up the targeted node won't lead to a multiparent situation, so we have to handle that, too. As a first pass:
+;;   Wow, that's convoluted. Okay, well, let's ignore the code to convert to
+;; and from the tree and concentrate on the actual manipulations. Specifically,
+;; let's look at swap-positions and push-node.
+;;   One thing to note is that we can actually merge these two functions. The
+;; "Find the thing with multiple parents" code will always turn up the node
+;; which was formerly the parent of the node we want to pull up to the top, so
+;; we can actually do away with that and directly resolve all three
+;; modifications in one step. Well, sort of - it's complicated by the fact that
+;; sometimes pulling up the targeted node won't lead to a multiparent situation,
+;; so we have to handle that, too. As a first pass:
 (swap-positions [target-key graph]
   (if (empty? ((graph target-key) :parents)) 
       graph
@@ -79,7 +94,10 @@
                                         :parents [parent-key]
                                         :children (remove #{parent-key} ((graph push-node-key) :children))))
             pulled-graph))))
-;; So, what's going on here is I merged the two functions. Now it always modifies the parent and the target to swap, and if there would be a node which would be multiparented, it handles the reparenting of that node in the (if push-node-key ...). It's very ugly.
+;;   So, what's going on here is I merged the two functions. Now it always
+;; modifies the parent and the target to swap, and if there would be a node
+;; which would be multiparented, it handles the reparenting of that node in the
+;; (if push-node-key ...). It's very ugly.
 (swap-positions [target-key graph]
   (if (empty? ((graph target-key) :parents)) 
       graph
@@ -99,7 +117,10 @@
                                         :parents [parent-key]
                                         :children (remove #{parent-key} ((graph push-node-key) :children))))
             pulled-graph))))
-;; I especially don't like that awkward (if push-node-key [push-node-key]) structure when I'm building the new parent. The thing is, it either goes there or we have to make the modification in the if proper, which - actually, hold on, let me see what that looks like with update-in:
+;;   I especially don't like that awkward (if push-node-key [push-node-key])
+;; structure when I'm building the new parent. The thing is, it either goes
+;; there or we have to make the modification in the if proper, which - actually,
+;; hold on, let me see what that looks like with update-in:
 (swap-positions [target-key graph]
             (if (empty? ((graph target-key) :parents)) 
                 graph
@@ -120,7 +141,9 @@
                                                                         ((graph push-node-key) :children))))
                           (update-in [parent-key :children] #(conj % push-node-key)))
                       pulled-graph))))
-;; Uh, hold on why am I doing this assoc thing when - wait, what. Those...those don't need to be assoc'd at all. I'm replacing them wholesale. Talking about modified-parent and modified-target, hold on:
+;;   Uh, hold on why am I doing this assoc thing when - wait, what. Those...those
+;; don't need to be assoc'd at all. I'm replacing them wholesale. Talking about
+;; modified-parent and modified-target, hold on:
 (swap-positions [target-key graph]
   (if (empty? ((graph target-key) :parents)) 
       graph
@@ -138,7 +161,13 @@
                         :children (remove #{parent-key} ((graph push-node-key) :children))})
                 (update-in [parent-key :children] #(conj % push-node-key)))
             pulled-graph))))
-;; It won't be winning any beauty contests, that's for sure. I considered an if-let, but then I'd just have to put a let anyways because for whatever arcane reason if-let allows one and only one binding, so that wouldn't get me any further prettiness. Still, this is a clunky way to solve the problem - converting it into a completely, utterly different internal representatnion, then converting it back at the end! Before we explore alternatives, let's plug this into the main body and count it up:
+;;   It won't be winning any beauty contests, that's for sure. I considered an
+;; if-let, but then I'd just have to put a let anyways because for whatever
+;; arcane reason if-let allows one and only one binding, so that wouldn't get me
+;; any further prettiness. Still, this is a clunky way to solve the problem -
+;; converting it into a completely, utterly different internal representatnion,
+;; then converting it back at the end! Before we explore alternatives, let's
+;; plug this into the main body and count it up:
 (fn bah [target tree]
   (letfn [(to-map-step [parent [key & children] m]
             (let [node [key {:parents (if parent [parent] [])
@@ -173,5 +202,11 @@
     (->> (to-map-step nil tree {}) 
          (swap-positions target)
          (to-list))))
-;; It's down to 34 lines, and in my opinion is...somewhat more readable. It's still not really readable, but you know, it's a step up from total incomprehensibility. It's also like 25% shorter!
-;; Aaaand, oh boy, I just looked at some of the solutions available online and I couldn't make heads nor tails of them on first inspection! So, here's what I think I'll do. I'll put this up as part 1, and then later, I'll come back and work my way through the solutions available online, see if I can't pick them apart, and compare them to my terrible, terrible solution.
+;;   It's down to 34 lines, and in my opinion is...somewhat more readable. It's
+;; still not really readable, but you know, it's a step up from total
+;; incomprehensibility. It's also like 25% shorter!
+;;   Aaaand, oh boy, I just looked at some of the solutions available online and
+;; I couldn't make heads nor tails of them on first inspection! So, here's what
+;; I think I'll do. I'll put this up as part 1, and then later, I'll come back
+;; and work my way through the solutions available online, see if I can't pick
+;; them apart, and compare them to my terrible, terrible solution.
